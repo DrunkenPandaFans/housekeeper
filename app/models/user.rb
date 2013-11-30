21fd1c25923ebecf78c6b1aeb3530a8f1842ffcf
Jan Ferko <julyloov@gmail.com>
2013-11-30 18:45:39 +0100

    User model refactored with active record pattern

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,50 +1,87 @@
 module Housekeeper
   class User
+    # Public: Username of user's Google account
+    attr_accessor :login
+
+    # Public: The email address listed on Google account
+    attr_accessor :email
+
+    # Public: The authentication token to Google Sign In
+    attr_accessor :token
+
+    # Public: Initialize a user.
+    #
+    # login - The String login of user's Google account
+    # email - The String email address of user's Google account.
+    # token - The GoogleToken authentication token to Google Sign In
+    def initialize(login, email, token)
+      @login = login
+      @email = email
+      @token = token
+    end
+
+    # Public: Returns all users
+    #
+    # Returns all users.
+    def self.all
+      Housekeeper::mongo["users"].find map do |data|
+        User.transform(data) 
+      end
+    end
+
+    # Public: Finds user
+    #
+    # Returns the User, or nill if user is not found.
+    def self.find(login)
+      login.downcase!
+      data = Housekeeper::mongo["users"].find({"_id" => login})
+      if data != nil
+        User.transform(data)
+      else
+        nil
+      end
+    end
+
+    # Public: Save the user
+    #
+    # Returns itself
+    def save
+      data = {"_id" => @login,
+              "email" => @email,
+              "google_token" => @token.to_hash}
+      Housekeeper::mongo["users"].insert(data)
+      self
+    end
+
+    private
+
+      def self.transform(user_data)
+        token = GoogleToken.new user_data["google_token"]
+        User.new user_data["_id"], user_data["email"], token
+      end
+  end
+
+  class GoogleToken
+    attr_accessor :refresh_token, :access_token, :expires_in, :issued_at
     
-    def initialize(database)
-      @db = database
+    def initialize(refresh_token, access_token, expires_in, issued_at)
+      @refresh_token = refresh_token
+      @access_token = access_token
+      @expires_in = expires_in
+      @issued_at = Time.at(issued_at)
     end
 
-    def save(user)
-      data = {"default_group" => user[:default_group],
-              "email" => user[:email],
-              "send_sms" => user[:send_sms],
-              "google_token" => user[:google_token]}
-
-      @db["users"].insert(data)
+    def self.create(data)
+      GoogleToken.new(data["refresh_token"], data["access_token"], 
+        data["expires_in"], data["issued_at"])
     end
 
-    def update(user)
-      data = {"_id" => user[:token],
-              "default_group" => user[:default_group],
-              "email" => user[:email],
-              "send_sms" => user[:send_sms],
-              "google_token" => user[:google_token]}
-      
-      @db["users"].update({"_id" => user[:token]}, data)
+    def to_hash
+      {"refresh_token" => @refresh_token,
+       "access_token" => @access_token,
+       "expires_in" => @expires_in,
+       "issued_at" => @issued_at.to_i}
     end
 
-    def remove(token)
-      @db["users"].remove("_id" => token)
-    end
-
-    def find(token)
-      data = @db["users"].find("_id" => token)
-      transform(data)
-    end
-
-    def find_all()
-      users = @db["users"].find.map(transform)
-    end
-
-  private
-
-    def transform(data)
-      {:token => data['_id'],
-       :default_group => data['default_group'],
-       :send_sms => data['send_sms'],
-       :email => data['email'],
-       :google_token => data['google_token']}
-    end
   end
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -4,28 +4,37 @@ require 'models/user'
 describe Housekeeper::User do
   
   before do
-    @db = mock()
+    db = mock()
     @collection = mock()
-    @db.expects(:[]).with("users").returns(@collection)
+    db.expects(:[]).with("users").returns(@collection)
+
+    Housekeeper.expects(:mongo).returns(db)
   end
 
   describe "should save user" do
     subject do
-      Housekeeper::User.new @db
+      token = Housekeeper::GoogleToken.new "abcsd", "accessthis", 1234, 765432109
+      Housekeeper::User.new "octocat", "octo@github.com", token
     end
     
     it "user call insert on 'users' collection" do
-      expectedDoc = {"send_sms" => true,
-                     "google_token" => "abcd",
-                     "email" => "my@email.com",
-                     "default_group" => "mygroup"}
-      @collection.expects(:insert).with(expectedDoc).once
-    
+      expectedDoc = {"_id" => "octocat",
+                     "email" => "octo@github.com",
+                     "google_token" => {
+                        "refresh_token" => "abcsd",
+                        "access_token" => "accessthis",
+                        "expires_in" => 1234,
+                        "issued_at" => 765432109
+                      }}
+      @collection.expects(:insert).with(expectedDoc).once    
 
-      subject.save({:send_sms => true,
-                    :google_token => "abcd",
-                    :email => "my@email.com",
-                    :default_group => "mygroup"})          
+      subject.save          
+    end
+
+    it "returns itself" do
+      @collection.expects(:insert).once
+      actual = subject.save
+      actual.must_equal subject
     end
   end
 
