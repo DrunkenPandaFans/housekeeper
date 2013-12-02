562be65fa7f11894009f8019d0c50905c4b723b6
Jan Ferko <julyloov@gmail.com>
2013-12-02 16:58:27 +0100

    Added sinatra main application

---
+++ app/app.rb
@@ -0,0 +1,21 @@
+require 'api/profile'
+
+module Play
+  class App < Sinatra::Base
+    
+    # Enable sessions and set session secret.
+    # So that in development mode sessions are not invalidated
+    enable :sessions
+    set    :session_secret, Housekeeper.config[:auth_token]
+
+    # Set folder with public assets
+    dir = File.dirname(File.expand_path(__FILE__))
+    set :public_folder, "#{dir}/frontend"
+    set :static, true
+
+    get "/" do
+      content_type :html
+      send_file('index.html')
+    end
+  end
+end
