ebddb5e4c70db0fa82276946a44d40db5a725c8c
Jan Ferko <julyloov@gmail.com>
2013-12-06 18:47:07 +0100

    App bootstrap configuration moved to configure method

--- app/app.rb
+++ app/app.rb
@@ -2,18 +2,20 @@ require 'api/profile'
 
 module Housekeeper 
   class App < Sinatra::Base
-    
-    # Enable sessions and set session secret.
-    # So that in development mode sessions are not invalidated
-    enable :sessions
-    set    :session_secret, Housekeeper.config[:auth_token]
 
-    use Housekeeper::Profile
+    configure do      
+      # load different parts of API
+      use Profile
+      # Enable sessions and set session secret.
+      # So that in development mode sessions are not invalidated
+      enable :sessions
+      set    :session_secret, 'asdf4fdsafas'
 
-    # Set folder with public assets
-    dir = File.dirname(File.expand_path(__FILE__))
-    set :public_folder, "#{dir}/frontend"
-    set :static, true
+      # Set folder with public assets
+      dir = File.dirname(File.expand_path(__FILE__))
+      set :public_folder, "#{dir}/frontend"
+      set :static, true
+    end
    
     get "/" do      
       redirect "index.html"  
