e99deba672cb73f0429e3f5f173008a7509647c9
Jan Ferko <julyloov@gmail.com>
2013-12-07 22:28:38 +0100

    Added user email retrieval to google service

--- app/services/google.rb
+++ app/services/google.rb
@@ -21,6 +21,23 @@ module Housekeeper
       client.execute!(plus.people.get, :userId => 'me').data
     end
 
+    # Public: Retrieve user email.
+    #
+    # token - The GoogleToken authentication token to access user's email address
+    #
+    # Returns user's email address
+    def self.user_email(token)
+      auth = build_auth
+      auth.update_token!(token.to_hash)
+
+      client = build_client
+      client.authorization = auth
+
+      oauth = client.discovered_api('oauth2', 'v2')
+
+      client.execute!(oauth.userinfo.get).data["email"]
+    end
+
     # Public: Retrieve user's Google Token.
     #
     # auth_code - The string authentication code, that can be exchange for token.
@@ -33,7 +50,7 @@ module Housekeeper
 
       begin
         data = auth.fetch_access_token
-      rescue ::Signet::AuthorizationError => err
+      rescue Signet::AuthorizationError => err
         data = nil
         # log error here
       end
@@ -41,7 +58,7 @@ module Housekeeper
       return nil if data == nil
 
       data["issued_at"] = Time.now.to_i
-      Housekeeper::GoogleToken.create(data)
+      GoogleToken.create(data)
     end
 
     private
@@ -62,7 +79,8 @@ module Housekeeper
           :token_credential_uri => Housekeeper::config[:google_token_uri],
           :client_id => Housekeeper::config[:client_id],
           :client_secret => Housekeeper::config[:client_secret],
-          :scope => 'https://www.googleapis.com/auth/plus.login',
+          :scope => 'https://www.googleapis.com/auth/plus.login' + 
+            'https://www.googleapis.com/auth/userinfo.email',
           :redirect_uri => 'postmessage'
       )
     end
