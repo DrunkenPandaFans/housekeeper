926c75e80ed352c78ab23e1cacdf5636427e7d10
Jan Ferko <julyloov@gmail.com>
2013-12-12 18:17:20 +0100

    Added token checking before api requests

--- app/app.rb
+++ app/app.rb
@@ -15,6 +15,28 @@ module Housekeeper
     dir = File.dirname(File.expand_path(__FILE__))
     set :public_folder, "#{dir}/frontend"
     set :static, true
+    
+    def api_request?
+      is_api_request = request.path_info =~ /\// ||
+                       request.path_info =~ /\/fonts/ ||
+                       request.path_info =~ /\/connect/
+      !is_api_request
+    end
+
+    before do
+      if api_request?
+        user_id = session[:user] || ""        
+        user = User.find(user_id)
+        
+        content_type :json
+
+        return halt 401, "Only authenticated users have access to this resource!" if !user
+
+        session[:user] = user_id
+      else
+        true
+      end
+    end
 
     get "/" do
       redirect "index.html"
