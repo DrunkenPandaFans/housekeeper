78631ed0441b10ff45232b88c0161247e4f81a05
Jan Ferko <julyloov@gmail.com>
2014-01-08 13:40:21 +0100

    Added Rack Test to test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,12 @@
+ENV['RACK_ENV'] = 'test'
+
 require 'simplecov'
 require 'coveralls'
 
 # load test libraries
 require 'minitest/autorun'
 require 'minitest/spec'
+require 'rack/test'
 require 'mocha/setup'
 
 # Start test coverage
