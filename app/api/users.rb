eff18e2b148e6d1bac674c629aa42400e43ef8c2
Jan Ferko <julyloov@gmail.com>
2014-02-18 23:11:31 +0100

    Removed get user by email in api

--- app/api/users.rb
+++ app/api/users.rb
@@ -1,20 +1,11 @@
 module Housekeeper
-  class Users < Sinatra::Base
-
-    get '/user/:email' do
-      email = params[:email]
-
-      user = Housekeeper::User.find_by_email(email)
-      
-      return halt 401, "User with this email does not exists" unless user
-      
-      prepare_user(user).to_json
-    end
+  class Users < Sinatra::Base    
 
     get "/user/:id" do
       id = params[:id]
 
       user = Housekeeper::User.find(id);
+      
       prepare_user(user).to_json
     end
 
