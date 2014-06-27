fea5ebcb01d7de01fd64c6e391684f4c94f1d6c8
Jan Ferko <julyloov@gmail.com>
2014-06-28 00:44:39 +0200

    Generated rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Housekeeper::Application.config.secret_key_base = 'b72b546df3b6469f617575788dfee258fa3e0d9c59e5d47bbe2c599347fa02d3b386fa9d665842a05f23f8b6781a42c149e3e6e974056f70e178d232def735c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Housekeeper::Application.config.session_store :cookie_store, key: '_housekeeper_session'
