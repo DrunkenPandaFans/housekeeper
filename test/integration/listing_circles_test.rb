cd129aef1e77b7baefba958c489812be5c797ab0
Jan Ferko <julyloov@gmail.com>
2014-06-29 14:00:00 +0200

    Refactored json parsing to test helper.

--- test/integration/listing_circles_test.rb
+++ test/integration/listing_circles_test.rb
@@ -12,7 +12,7 @@ class ListingShoppingListsTest < ActionDispatch::IntegrationTest
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
 
-    assert_equal Circle.count, JSON.parse(response.body).size
+    assert_equal Circle.count, json(response.body).size
   end
 
   test 'lists circles by name' do
@@ -21,6 +21,6 @@ class ListingShoppingListsTest < ActionDispatch::IntegrationTest
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
 
-    assert_equal 1, JSON.parse(response.body).size
+    assert_equal 1, json(response.body).size
   end
 end
