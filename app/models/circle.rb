371d1e55923218d921eb23486e2a0b0a503b980d
Jan Ferko <julyloov@gmail.com>
2014-06-30 15:36:52 +0200

    Added circle validations

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -1,2 +1,3 @@
 class Circle < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
 end
--- test/models/circle_test.rb
+++ test/models/circle_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CircleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    Circle.create!(name: "Amazon", description: "Amazon shopping circle")
+  end
+
+  test "save circle without name" do
+    circle = Circle.new(name: "", description: "Empty circle")
+    assert circle.invalid?
+  end
+
+  test "save circle with existing name" do
+    circle = Circle.new(name: "Amazon", description: "Another amazon shopping circle")
+    assert circle.invalid?
+  end
 end
