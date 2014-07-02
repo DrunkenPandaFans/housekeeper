4da2fda0046097d89f9219be43ca1ef76fbd09a8
Jan Ferko <julyloov@gmail.com>
2014-07-02 18:58:40 +0200

    Created api subdomain

---
+++ app/controllers/api/circles_controller.rb
@@ -0,0 +1,15 @@
+module API
+  class CirclesController < ApplicationController
+    def index
+      circles = Circle.all
+      name = params[:name]
+
+      if name
+        circles = circles.where("lower(name) like ?",
+          "%#{name.downcase}%")
+      end
+
+      render json: circles, status: 200, root: false
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Housekeeper::Application.routes.draw do
-  with_options only: [:index, :show, :update] do |list_only|
-    list_only.resources :circles
-    list_only.resources :users
+
+  namespace :api, path: '/',  constraint: { subdomain: 'api' } do
+    with_options only: [:index, :show, :update] do |list_only|
+      list_only.resources :circles
+      list_only.resources :users
+    end
+
+    get '/user', to: 'users#show'
+    patch '/user', to: 'users#update'
   end
 
-  get '/user', to: 'users#show'
-  patch '/user', to: 'users#update'
 end
