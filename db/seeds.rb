a68a5b2305c460bb875cfe68a1b65480f10f952f
Jan Ferko <julyloov@gmail.com>
2014-07-02 16:26:52 +0200

    Added users to db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,19 @@ Circle.create([{
   name: 'Local Albert shopping',
   description: 'Buying generic quality in Albert'
 }])
+
+User.create([{
+  name: 'Jan',
+  email: 'jan@email.com',
+  token: 'abcdefg1234',
+  send_sms: true,
+  image: 'http://awesomeurl.com/jan.png'
+},
+{
+  name: 'Octocat',
+  email: 'octocat@email.com',
+  token: 'abcdefg',
+  send_sms: true,
+  image: 'http://awesomeurl.com/octocat.png'
+}])
+
