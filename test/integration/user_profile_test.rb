8a362fdb469bda8d784fdf90939654200f271de0
Jan Ferko <julyloov@gmail.com>
2014-07-05 01:19:15 +0200

    Separated different user integration tests to new test class

--- test/integration/user_profile_test.rb
+++ test/integration/user_profile_test.rb
@@ -4,8 +4,6 @@ class UserProfileTest < ActionDispatch::IntegrationTest
   setup do
     @jan = User.create!(name: 'Jan', email: 'jan@drunkenpanda.com', token: '123456abcdefg', send_sms: false,
       image: 'http://someawesomeimages.com/jan.png')
-    User.create!(name: 'Octocat', email: 'octocat@github.com', token: 'abcde', send_sms: false,
-      image: 'http://github.com/images/octocat.png')
   end
 
   test "get authenticated user profile" do
@@ -27,34 +25,4 @@ class UserProfileTest < ActionDispatch::IntegrationTest
     assert_equal "Bad credentials.", error[:error]
   end
 
-  test "get profile of different user" do
-    get "/users/#{@jan.id}", {}, {"Authorization" => token_auth("abcde")}
-
-    assert_equal 200, response.status
-    assert_equal Mime::JSON, response.content_type
-
-    user = json(response.body)
-    assert_equal @jan.name, user[:name]
-  end
-
-  test "get profile of user without authentication" do
-    get "/users/#{@jan.id}"
-
-    assert_equal 401, response.status
-    assert_equal Mime::JSON, response.content_type
-
-    error = json(response.body)
-    assert_equal "Bad credentials.", error[:error]
-  end
-
-  test "get profile of nonexistent user" do
-    get "/users/-12323", {},
-      {"Authorization" => token_auth(@jan.token)}
-
-    assert_equal 404, response.status
-    assert_equal Mime::JSON, response.content_type
-
-    error = json(response.body)
-    assert_equal "User not found.", error[:error]
-  end
 end
