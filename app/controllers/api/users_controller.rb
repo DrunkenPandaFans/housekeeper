5eb9b86d36e2f518eb58e44978a8177471c1ad8d
Jan Ferko <julyloov@gmail.com>
2014-11-04 13:35:36 +0100

    Added auth controller for api

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,5 @@
 module API
-  class UsersController < ApplicationController
-    before_action :authenticate
-
+  class UsersController < AuthenticationController
     def index
       users = User.all
       render json: users, status: 200, root: false
@@ -30,25 +28,5 @@ module API
       end
     end
 
-    protected
-
-      def authenticate
-        authenticate_by_token || render_unauthorized
-      end
-
-      def authenticate_by_token
-        authenticate_with_http_token do |token, options|
-          @current_user = User.find_by(token: token)
-        end
-      end
-
-      def render_unauthorized
-        self.headers["WWW-Authenticate"] = 'Token realm="Users"'
-        render json: { error: "Bad credentials."}, status: 401
-      end
-
-      def user_params
-        params.permit(:name, :email, :image, :send_sms)
-      end
   end
 end
